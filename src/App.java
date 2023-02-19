22fdf26e7d32da5067fe3ca59ee7b419367b51cb
fqqk <fqqk.work68@gmail.com>
2023-02-19 19:44:50 +0900

    順列チェック1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import data_structure.Sort;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean result = Sort.permutation("hello", "heoll");
+        String sort_result = Sort.sort("faafjal");
+        System.out.println(result);
+        System.out.println(sort_result);
     }
 }
---
+++ src/data_structure/Sort.java
@@ -0,0 +1,30 @@
+package data_structure;
+
+public class Sort {
+  String s;
+  String t;
+  
+  // static methodはrubyでいうところのクラスメソッド。インスタンス化せずに直接呼び出せる
+  public static String sort(String s) {
+    char[] content = s.toCharArray();
+    java.util.Arrays.sort(content);
+    return new String(content);
+  }
+
+  public static boolean permutation(String s, String t) {
+    if(s.length() != t.length()){
+      return false;
+    }
+    return sort(s).equals(sort(t));
+  }
+
+  // Java実行に必須のメソッド
+  public static void main(String[] args) throws Exception {
+    boolean result1 = Sort.permutation("hello", "heoll");
+    boolean result2 = Sort.permutation("Hello", "heoll");
+    boolean result3 = Sort.permutation("hello ", "heoll ");
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+  }
+}
